a03a73f9ce889b96bc0871ad60b0b20f3ffd95eb
Administrator <admin@example.com>
2019-06-02 23:11:41 +0900

    Initial Commit

---
+++ perm/command.py
@@ -0,0 +1,14 @@
+"""
+    The command line goes here
+"""
+
+import click
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+
+@cli.command()
+def sync():
+    click.echo('Synching')
